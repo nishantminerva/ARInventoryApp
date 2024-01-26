64aa987efbbbfdf76c6af65eaad1d931944c3e33
Nishant Minerva <nishantcafe@gmail.com>
2024-01-26 20:51:14 +0530

    Initial Commit

---
+++ ARInventoryApp/ARInventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARInventoryAppApp.swift
+//  ARInventoryApp
+//
+//  Created by Nishant Minerva on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ARInventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
